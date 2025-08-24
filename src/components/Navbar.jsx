0ae8e458fe8bd17416ec2dc69afb08f295ced8df
Esteban <esteban.barbosa.2001@gmail.com>
2025-08-24 11:35:30 +0200

    Fix Bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 function Navbar() {
   return (
@@ -7,8 +8,8 @@ function Navbar() {
         <h1>Mon Portfolio "Esteban"</h1>
         <ul>
           <li><Link to="/">Accueil</Link></li>
-          <li><a href="#about">À propos</a></li>
-          <li><a href="#skills">Compétences</a></li>
+          <li><HashLink smooth to="/#about">À propos</HashLink></li>
+          <li><HashLink smooth to="/#skills">Compétences</HashLink></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
